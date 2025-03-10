a4855512e251bf30a1030d72586c928fc382300b
ml-mahendra <ml.mahendranath@gmail.com>
2025-03-10 10:14:09 +0000

    Ex #1

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+""" Exercise #1 : Hello, World! """
+
+print("Hello, world!")
+print("What is your name?")
+name = input()
+print("Hello, "+name)
+
+# can replce the 2nd and 3rd line with, name = input("What is your name?")
